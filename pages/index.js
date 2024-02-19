a3fb0a211dfd79df734b5a75930d876c4cfae5a5
Riccardo Papini <rickypapini@gmail.com>
2024-02-19 16:37:39 +0200

    Added inital header element. Will come back to fix styling and clean up

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Head>
+        <title>Airbnb 2.0</title>
+      </Head>
+
+      <Header/>
+      
+      {/* Banner */}
+
+    </>
   )
 }
